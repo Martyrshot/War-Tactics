7590f4aa9fbfa6d62f841e388384b8d6ecc71889
Jason Goertzen <Jason@goertzen.ch>
2020-03-26 01:22:51 -0600

    Bare board is printing, but doesn't actually use board state currently

--- client.cpp
+++ client.cpp
@@ -7,20 +7,39 @@ using namespace std;
 // etherium address
 string address;
 
+// store the current board so we don't have to keep asking for it
 uint8_t board[3][10][9];
 
+// current hand state
 vector<int> handIDs;
+
+#define BOARDHORIZONTALBAR "+---+---+---+---+---+---+---+---+---+---+"
+#define BOARDHANDSEPARATER "========================================="
+void printBoard(void) {
+    cout << BOARDHANDSEPARATER << endl;
+    cout << BOARDHORIZONTALBAR << endl;
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << "|   ";
+        }
+        cout << "| " << i << endl << BOARDHORIZONTALBAR << endl;
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << "  " << (char)(i + 65) << " ";
+    }
+    cout << endl << BOARDHANDSEPARATER << endl;
+}
+
 
 int main(int argc, char **argv) {
     vector<int> v;
     for (int i = 0; i < 52; i++) {
         if (i % 9 == 2) {
-            cout << i << endl;
             v.push_back(i);
         } 
     }
 
-    vector< vector<string> > hand = idsToASCII(v);
-    printHand(hand);
+    printBoard();
+    printHand(v);
     return 0;
 }
